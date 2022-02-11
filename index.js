0e0c51ef0f7b1e807120a649dabe9e21b8569a06
“chrisdotjs” <chrisdotjs@gmail.com>
2022-02-11 23:55:21 +0100

    cors false

--- index.js
+++ index.js
@@ -14,12 +14,14 @@ async function startApolloServer() {
         resolvers
     })
     await apolloServer.start()
-    apolloServer.applyMiddleware({ app })
+    apolloServer.applyMiddleware({ app, cors: false })
     app.use(cors({
         optionsSuccessStatus: 200,
+        origin: '*',
 //        origin: "http://localhost:3000",
-        origin: "https://01-client-netlify.netlify.app/",
-//        origin: "https://chrisdotjs.github.io/01-client-netlify/"
+//        origin: "https://01-client-netlify.netlify.app/",
+//        origin: "https://chrisdotjs.github.io/01-client-netlify/",
+        credentials: true
     }))
     dotenv.config()
     app.use((req, res) => { res.send("Express server is running!")})
